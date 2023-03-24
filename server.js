db79e61ed7a62210815ff61935fd6d29748bf823
Daniel Kiss <danielkiss@Daniels-MacBook-Pro.local>
2023-03-24 11:03:44 -0700

    add express server

---
+++ server.js
@@ -0,0 +1,11 @@
+import express from 'express'
+// const express = require('express')
+const app = express()
+
+app.get("/", (req, res) => {
+    res.send('Welcome')
+})
+
+const port = process.env.PORT || 5001
+
+app.listen(port, () => { console.log(`Server is listening on port ${port}`) })
