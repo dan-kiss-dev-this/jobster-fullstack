8dbd1c5f22915aad48a54dc5de2bb034dbc090a2
Daniel Kiss <danielkiss@Daniels-MacBook-Pro.local>
2023-03-27 17:01:50 -0700

    add email validation

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import validator from 'validator'
 
 const UserSchema = new mongoose.Schema({
     name: {
@@ -11,6 +12,10 @@ const UserSchema = new mongoose.Schema({
     email: {
         type: String,
         required: [true, "please provide name"],
+        validate: {
+            validator: validator.isEmail,
+            message: "Please provide a valid email"
+        },
         // this is not a validator technically using mongoose index
         unique: true
     },
