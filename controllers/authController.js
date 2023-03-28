0c8993d7f48500e8f9c5516fe171439f2e084b73
Daniel Kiss <danielkiss@Daniels-MacBook-Pro.local>
2023-03-28 14:26:51 -0700

    add status codes package

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,11 @@
 import User from "../models/User.js"
+import { StatusCodes } from 'http-status-codes'
 
 const register = async (req, res, next) => {
     // user object shows up here and is loaded to mongodb, res is given
     try {
         const user = await User.create(req.body)
-        res.status(201).json({ user })
-        console.log(99, user)
+        res.status(StatusCodes.OK).json({ user })
     } catch (error) {
         // we use next instead of this code to pass the error to the next middleware res.status(500).json({ msg: "there was an error" })
         next(error)
