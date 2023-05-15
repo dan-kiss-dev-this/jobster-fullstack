29d2a4425675d51601fa9eef215a71db582d28d1
Daniel Kiss <danielkiss@MacBook-Pro.local>
2023-05-15 11:57:41 -0700

    add components to shared layout, add react-icons library

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,5 +2,8 @@ import Logo from "./Logo"
 import TestSample from "./TestSample"
 import FormRow from "./FormRow"
 import Alert from "./Alert"
+import BigSidebar from "./BigSidebar"
+import SmallSidebar from './SmallSidebar'
+import Navbar from './Navbar'
 
-export { Logo, TestSample, FormRow, Alert }
+export { Logo, TestSample, FormRow, Alert, BigSidebar, SmallSidebar, Navbar }
--- client/src/pages/dashboard/SharedLayout.js
+++ client/src/pages/dashboard/SharedLayout.js
@@ -1,16 +1,28 @@
 import React from 'react'
 import { Outlet, Link } from 'react-router-dom';
 import Wrapper from '../../assets/wrappers/SharedLayout'
+import { BigSidebar, SmallSidebar, Navbar } from "../../components/"
 
 const SharedLayout = () => {
     return (
         <Wrapper>
+            <main className="dashboard">
+                {/* css is used to show and hide sizebar based on 992px screen size */}
+                <SmallSidebar ></SmallSidebar>
+                <BigSidebar></BigSidebar>
+                <div>
+                    <Navbar></Navbar>
+                    <div className="dashboard-page">
+                        <Outlet />
+                    </div>
+                </div>
+            </main>
             {/* note the nav stays on all pages and portions are swapped based on the outlet */}
-            <nav>
+            {/* <nav>
                 <Link to="all-jobs">all jobs</Link>
                 <Link to="add-job">add job</Link>
-            </nav>
-            <Outlet />
+            </nav> */}
+
         </Wrapper>
     )
 }
