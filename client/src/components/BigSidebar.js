518ea9967e56c4aac664c7967a790082455bc87b
Daniel Kiss <danielkiss@MacBook-Pro.local>
2023-05-16 10:35:45 -0700

    setup small sidebar

--- client/src/components/BigSidebar.js
+++ client/src/components/BigSidebar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Wrapper from '../assets/wrappers/BigSidebar'
+import { NavLink } from 'react-router-dom';
 
 const BigSidebar = () => {
     return (
--- client/src/components/SmallSidebar.js
+++ client/src/components/SmallSidebar.js
@@ -1,10 +1,45 @@
 import React from 'react';
 import Wrapper from '../assets/wrappers/SmallSidebar'
+import { FaTimes } from 'react-icons/fa'
+import { useAppContext } from '../context/appContext';
+import links from '../utils/links'
+import { NavLink } from 'react-router-dom';
+import Logo from './Logo'
+
+import { Link } from 'react-router-dom';
 
 const SmallSidebar = () => {
+    const { showSidebar, toggleSidebar } = useAppContext()
     return (
         <Wrapper>
-            <h4>small sidebar</h4>
+            <div className={showSidebar ? "sidebar-container show-sidebar" : "sidebar-container"}>
+                <div className="content">
+                    <button type="button" className="close-btn" onClick={toggleSidebar}>
+                        <FaTimes />
+                    </button>
+                    <header>
+                        <Logo />
+                    </header>
+                    <div className="nav-links">
+                        {links.map((link) => {
+                            const { id, text, path, icon } = link;
+                            return (<NavLink to={path}
+                                // isActive prop provided by NavLink
+                                className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}
+                                key={id}
+                                onClick={toggleSidebar}
+                            >
+                                <span className="icon">{icon}</span>
+                                {text}
+                            </NavLink>)
+                        })}
+                    </div>
+                    {/* <div className="nav-links">nav links</div> */}
+                </div>
+
+
+
+            </div>
         </Wrapper>
     )
 }
