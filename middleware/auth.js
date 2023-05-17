ed1f425493a9f15a5ae7396983d40c16c377f765
Daniel Kiss <danielkiss@MacBook-Pro.local>
2023-05-16 20:30:46 -0700

    update auth for error handling if not authorized

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,17 @@
+import { UnAuthenticatedError } from '../errors/index.js'
+
 const auth = async (req, res, next) => {
     console.log('authenticate user')
     const authHeader = req.headers.authorization
     console.log(66, authHeader)
+
+    if (!authHeader) {
+        // 401 error
+        // return res.status(401).json({ message: 'Invalid Credentials' })
+        // use error object
+        throw new UnAuthenticatedError('Authentication Invalid')
+    }
+    // next() means you are passed to the next middleware
     next()
 }
 
