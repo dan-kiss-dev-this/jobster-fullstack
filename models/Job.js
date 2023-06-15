af76a62d8663f24218061bd6048c66287f4af908
Daniel Kiss <danielkiss@MacBook-Pro.local>
2023-06-15 16:36:21 -0700

    create a job model with mongoose odm

---
+++ models/Job.js
@@ -0,0 +1,44 @@
+import mongoose from 'mongoose';
+import validator from 'validator'
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+
+const JobSchema = new mongoose.Schema(
+    {
+        company: {
+            type: String,
+            required: [true, "please provide company"],
+            maxlength: 50,
+        },
+        position: {
+            type: String,
+            required: [true, "please provide position"],
+            maxlength: 100,
+        },
+        status: {
+            type: String,
+            enum: ['interview', 'declined', 'pending'],
+            default: 'pending',
+        },
+        jobType: {
+            type: String,
+            enum: ['full-time', 'part-time', 'remote', 'internship'],
+            default: 'full-time',
+        },
+        jobLocation: {
+            type: String,
+            default: 'my city',
+            required: [true]
+        },
+        createdBy: {
+            type: mongoose.Types.ObjectId,
+            ref: 'User',
+            required: [true, 'Please provide user']
+        }
+    },
+    {timestamps: true}
+);
+
+
+// to create the jobs collection in mongodb, note model has a type and schema for it
+export default mongoose.model('Job', JobSchema)
