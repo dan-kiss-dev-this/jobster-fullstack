436c02aaf4d41474973fa66982966b680e235012
Daniel Kiss <danielkiss@MacBook-Pro.local>
2023-06-15 17:51:40 -0700

    add to jobsController to createJob in mongodb

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,6 +1,19 @@
+import Job from '../models/Job.js'
+import { StatusCodes } from 'http-status-codes'
+import { BadRequestError, NotFoundError } from '../errors/index.js'
+
 // async due to communication with the database. 
 const createJob = async (req, res) => {
-    res.send('create job')
+    // note jobLocation, jobStatus, jobType can use the default values
+    const { company, position } = req.body
+    //idea use the company and position to make a new job in mongodb
+    if (!position || !company) {
+        throw new BadRequestError('Please Provide All Values')
+    }
+    // the auth middleware is adding the user id to the request object automatically
+    req.body.createdBy = req.user.userId
+    const job = await Job.create(req.body)
+    res.status(StatusCodes.CREATED).json({ job })
 }
 
 const getAllJobs = async (req, res) => {
