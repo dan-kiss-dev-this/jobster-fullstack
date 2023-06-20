4951662bc7f50869bc4f472b09ee832cf287e058
Daniel Kiss <danielkiss@MacBook-Pro.lan>
2023-06-20 13:49:14 -0700

    add way to edit existing job

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -1,5 +1,5 @@
 import React, { useReducer, useContext } from 'react';
-import { DISPLAY_ALERT, CLEAR_ALERT, REGISTER_USER_BEGIN, REGISTER_USER_SUCCESS, REGISTER_USER_ERROR, LOGIN_USER_BEGIN, LOGIN_USER_SUCCESS, LOGIN_USER_ERROR, TOGGLE_SIDEBAR, LOGOUT_USER, UPDATE_USER, UPDATE_USER_BEGIN, UPDATE_USER_SUCCESS, UPDATE_USER_ERROR, HANDLE_CHANGE, CLEAR_VALUES, CREATE_JOB_BEGIN, CREATE_JOB_SUCCESS, CREATE_JOB_ERROR, GET_JOBS_BEGIN, GET_JOBS_SUCCESS, SET_EDIT_JOB, DELETE_JOB_BEGIN } from './actions';
+import { DISPLAY_ALERT, CLEAR_ALERT, REGISTER_USER_BEGIN, REGISTER_USER_SUCCESS, REGISTER_USER_ERROR, LOGIN_USER_BEGIN, LOGIN_USER_SUCCESS, LOGIN_USER_ERROR, TOGGLE_SIDEBAR, LOGOUT_USER, UPDATE_USER, UPDATE_USER_BEGIN, UPDATE_USER_SUCCESS, UPDATE_USER_ERROR, HANDLE_CHANGE, CLEAR_VALUES, CREATE_JOB_BEGIN, CREATE_JOB_SUCCESS, CREATE_JOB_ERROR, GET_JOBS_BEGIN, GET_JOBS_SUCCESS, SET_EDIT_JOB, DELETE_JOB_BEGIN, EDIT_JOB_BEGIN, EDIT_JOB_SUCCESS, EDIT_JOB_ERROR } from './actions';
 import Reducer from './reducer'
 import axios from 'axios'
 
@@ -243,11 +243,22 @@ const AppProvider = ({ children }) => {
     const editJob = async (id) => {
         console.log('edit job')
         try {
+            const { position, company, jobLocation, jobType, status } = state;
+            await authFetch.patch(`/jobs/${state.editJobId}`, {
+                position, company, jobLocation, jobType, status
+            })
+            dispatch({ type: EDIT_JOB_SUCCESS })
+            dispatch({ type: CLEAR_VALUES })
             // can do a dispatch to disable button when loading, send in job with new values, need whole job including id, send id to endpoint with job in the payload, something like update job success. have a consistent pattern
             // let job = await authFetch.patch(`/jobs/${id}`, {})
         } catch (error) {
-            // dispatch something sayng there was an error
+            if (error.response.status === 401) return
+            dispatch({
+                type: EDIT_JOB_ERROR,
+                payload: { MSG: error.response.data.msg }
+            })
         }
+        clearAlert()
     }
 
     const deleteJob = async (id) => {
