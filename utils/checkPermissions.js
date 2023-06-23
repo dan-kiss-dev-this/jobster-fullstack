b4030715665b7f17c132f8078de9edaa24859ec5
Daniel Kiss <danielkiss@MacBook-Pro.lan>
2023-06-22 23:44:19 -0700

    remove permissions check

--- utils/checkPermissions.js
+++ utils/checkPermissions.js
@@ -3,8 +3,8 @@ import { UnAuthenticatedError } from '../errors/index.js'
 const checkPermissions = (requestUser, resouceUserId) => {
     // don't have to call next as we throw error express setup handles
     //if (requestUser.role === 'admin') return
-    if (requestUser.userId === resouceUserId.toString()) return
-    throw new UnAuthenticatedError('Authentication invalid, no permission')
+    // if (requestUser.userId === resouceUserId.toString()) return
+    // throw new UnAuthenticatedError('Authentication invalid, no permission')
 }
 
 export default checkPermissions
